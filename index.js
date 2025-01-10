b91fa219e14b9d00fec7de44668963f7654e0d4d
Kaspeyy <kasp4593@gmail.com>
2025-01-10 18:09:41 +0000

    Update index.js

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ app.listen(port, () => {
   console.log('\x1b[36m[ SERVER ]\x1b[0m', '\x1b[32m SH : http://localhost:' + port + ' ✅\x1b[0m');
 });
 
-const statusMessages = ["🌊 Managing Capital Area Transit"];
+const statusMessages = ["Watching over 200+ members"];
 const statusTypes = [ 'dnd', 'idle'];
 let currentStatusIndex = 0;
 let currentTypeIndex = 0;
